1b2e1631c6e6519829900051465920a7bb050e3b
aloginow <arthur.loginow@comprandoengrupo.net>
2021-07-31 22:20:42 -0300

    add shorter code, more elegant and less verbose

--- kyu-6/split-strings.js
+++ kyu-6/split-strings.js
@@ -1,11 +1,5 @@
-function solution(str){    
-    const splitStrings = [];
-    
-    for (let i = 0; i < str.length; i+= 2) {
-        i == (str.length - 1) && str.length % 2 != 0 ? 
-        splitStrings.push(str[i]+'_') : 
-        splitStrings.push(str[i]+str[i+1])
-    }
-    
-    return splitStrings
-  }
+function solution(str) {    
+  const splitStrings = [];
+  for (let i = 0; i < str.length; i+= 2) splitStrings.push(str[i]+(str[i+1] || '_'))
+  return splitStrings
+}
